994e19cc6aca6adae4d356e2e048695df74d47ee
Gardens123-cell <114577723+Gardens123-cell@users.noreply.github.com>
2023-03-30 23:02:10 +0400

    L7

---
+++ L7.py
@@ -0,0 +1,12 @@
+try:
+    numbers = int(input('Insert number: '))
+    print('Ukazannoye chislo',numbers)
+except:
+    print('Mi nashli oshibku')
+print('Hello world')
+
+# numbers = int(input('Insert number: '))
+# print('Ukazannoye chislo', numbers)
+# print('Hello world')
+
+
---
+++ L8.py
@@ -0,0 +1,116 @@
+# oper = input("Select operation: /, *, +, -: ")
+#
+#
+# class Calc:
+#
+#     def __init__(self):
+#         self.num1 = int(input('Enter 1st number: '))
+#         self.num2 = int(input('Enter 1st number: '))
+#
+#
+#     def sum(self):
+#          return (self.num1 + self.num2)
+#
+#
+#     def extr(self):
+#         return (self.num1 - self.num2)
+#
+#     def multp(self):
+#         return (self.num1 * self.num2)
+#
+#     def div(self):
+#
+#         return self.num1 / self.num2
+#
+#
+#
+#
+#
+#
+# res = Calc()
+#
+# if oper == '+':
+#     print('Result:',res.sum())
+#
+# if oper == '-':
+#     print('Result:', res.extr())
+#
+# if oper == '*':
+#     print('Result:',round(res.multp()))
+#
+# try:
+#     if oper == '/':
+#         print( self.num1 / self.num2)
+# except:
+#     print('Error')
+#
+#
+#
+#
+#
+#
+# 2 dop
+#
+#
+#
+#
+#
+#
+# class Animals:
+#     def __init__(self):
+#         print('Colour of pokemon: ',self.colour)
+#         print('Name: ',self.name)
+#         print('Age: ',self.age)
+#         print('Height: ',self.height)
+#         print('Power: ',self.power)
+#         print('--------------------------------')
+#
+# class Fire(Animals):
+#     name = 'Ash Ketchum'
+#     colour = "Red"
+#     age = 1500
+#     height = '55m'
+#     power = 'Fire'
+#
+# class Water(Animals):
+#     name = "Pikachu"
+#     colour = "Black"
+#     age = 1000
+#     height = '22m'
+#     power = 'Water'
+#
+# class Lightning(Animals):
+#     name = "Brock"
+#     colour = "Brown"
+#     age = 500
+#     height = '444m'
+#     power = 'Lightning'
+#
+# class Ground(Animals):
+#     name = "Drawn"
+#     colour = "White"
+#     age = 1754
+#     height = '754m'
+#     power = 'Ground'
+# class evil(Animals):
+#     name = 'Pika'
+#     colour = 'Blue'
+#     age = 144
+#     height = '415m'
+#     power = 'Evil'
+#
+# poke = Fire()
+# poke = Water()
+# poke = Lightning()
+# poke = Ground()
+# poke = evil()
+
+
+
+
+
+class Human
+    def __init__(self,name,age,avrg):
+        self.name = name
+        self.age = age
+        self.avrg = avrg
