0319b14e9c0d461fb8c407aa7284fdbcac1102fc
Gardens123-cell <114577723+Gardens123-cell@users.noreply.github.com>
2023-03-31 23:36:48 +0400

    Dz 7.1

---
+++ Dz7.1.py
@@ -0,0 +1,28 @@
+import random
+list = [random.randint(-100,100) for i in range(50)]
+print(list)
+
+pos = 0
+neg = 0
+zero = 0
+
+num = 0
+
+max = max(list)
+min = min(list)
+
+for num in list:
+    if num>0:
+        pos +=1
+
+    else:
+        neg +=1
+
+    if num==0:
+        zero +=1
+
+print(('Max number: '),max)
+print('Min number: ',min)
+print("Positive numbers in the list: ", pos)
+print("Negative numbers in the list: ", neg)
+print('Zeros: ',zero)
